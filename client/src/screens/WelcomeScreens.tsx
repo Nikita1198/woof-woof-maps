19fc053dc7711a9aba6724f54fa3d024549533c0
nivanov <nivanov@pari.ru>
2024-07-12 11:06:24 +0300

    Update url api

--- client/src/screens/WelcomeScreens.tsx
+++ client/src/screens/WelcomeScreens.tsx
@@ -202,7 +202,7 @@ const MainScreens = () => {
 
   const fetchTokenFromBot = async (userId) => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/get_token", {
+      const response = await fetch("http://90.156.209.225:5000/get_token", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -220,14 +220,17 @@ const MainScreens = () => {
   // Function to fetch user info
   const fetchUserInfo = async (userId, token) => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/get_user_info", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "x-access-tokens": token,
-        },
-        body: JSON.stringify({ user_id: userId }),
-      });
+      const response = await fetch(
+        "http://90.156.209.225:5000/api/get_user_info",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "x-access-tokens": token,
+          },
+          body: JSON.stringify({ user_id: userId }),
+        }
+      );
       const data = await response.json();
       console.log(data); // Используйте данные пользователя по мере необходимости
     } catch (error) {
