1fcee5afc0794a6d06bc62c34af697e1ccc89e90
nivanov <nivanov@pari.ru>
2024-07-13 22:58:04 +0300

    height

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -4,6 +4,7 @@ const Logo = memo(() => (
   <img
     src="../aphrodita_logo.png"
     width={170}
+    height={170}
     alt="Logo"
     loading="lazy"
     style={{ display: "block", margin: "0 auto" }}
